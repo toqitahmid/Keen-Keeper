584dbfdba6522057dcd12e6a82793062ca7f349e
toqitahmid <rubaiathali@gmail.com>
2026-05-20 01:07:33 +0600

    secure the setup

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: [
+    "192.168.56.1", 
+    "192.168.56.1:3000",
+  ],
 };
 
 export default nextConfig;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
+import FooterPage from "./components/Footer";
+import { InteractionProvider } from "@/context/InteractionContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,9 +23,20 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
+      data-theme = 'light'
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body 
+      suppressHydrationWarning
+      className="min-h-full flex flex-col">
+        <InteractionProvider>
+      <Navbar></Navbar>
+      <main className="flex-1">
+      {children}
+      </main>
+      <FooterPage></FooterPage>
+        </InteractionProvider>
+      </body>
     </html>
   );
 }
